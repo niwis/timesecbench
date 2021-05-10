0fe374c08268a263300e58ec9607ce07cda8364a
Pierre Jamelot <pierre.jamelot@inria.fr>
2021-05-10 11:16:56 +0200

    wip clock

--- config/armv8a/boards/rpi3/boardsupport.h
+++ config/armv8a/boards/rpi3/boardsupport.h
@@ -18,20 +18,47 @@
 #define CPU_MHZ 1
 typedef unsigned int uint32_t;
 
+readticks(unsigned int *result, int enabled)
+{
+    struct timeval t;
+    unsigned int cc;
+    unsigned int val;
+    if (!enabled) {
+        // program the performance-counter control-register:
+        asm volatile("msr pmcr_el0, %0" : : "r" (17));
+        //enable all counters
+        asm volatile("msr PMCNTENSET_EL0, %0" : : "r" (0x8000000f));
+        //clear the overflow 
+        asm volatile("msr PMOVSCLR_EL0, %0" : : "r" (0x8000000f));
+        enabled = 1;
+    }
+    //read the coutner value
+    asm volatile("mrs %0, PMCCNTR_EL0" : "=r" (cc));
+    gettimeofday(&t,(struct timezone *) 0);
+    result[0] = cc;
+    result[1] = t.tv_usec;
+    result[2] = t.tv_sec;
+
 volatile inline uint32_t read_time() {
-    struct timespec stop;
-    struct timespec start;
-    float exec_time;
-    uint32_t time;
-    clock_gettime(CLOCK_MONOTONIC, &start); 
-    const clock_t start_tick = clock(); 
-    clock_t current_tick = clock();
-    clock_gettime(CLOCK_MONOTONIC, &stop); 
-    printf("II. Current tick - start to: %u\n", (unsigned int)(current_tick-start_tick));
-    exec_time = ( stop.tv_sec - start.tv_sec ) + ( stop.tv_nsec - start.tv_nsec ) / 1000000000.0;
-    printf("II. Sleep took time: %f\n", exec_time);
-    // __asm__ volatile ("rdcycle %[time]" : [time] "=r" (time) : : );
-    return time;
+    unsigned int init[3] = {0};
+    unsigned int start[3] = {0};
+    unsigned int end[3] = {0};
+    unsigned int overhead = 0;
+
+    readticks(init, 0);
+    readticks(start, 1);
+    readticks(end, 1);
+
+    overhead = end[0] - start[0];
+    readticks(init, 0);
+    readticks(start, 1);
+    foo(); //This is my function 
+    readticks(end, 1);
+
+    end[0] = end[0] - start[0] - overhead;
+    printf("clock cycles= %d\n", end[0]);
+    return 0;
+return time;
 }
 
 
