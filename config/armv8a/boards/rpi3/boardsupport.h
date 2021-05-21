0c654a214992cef2c4dd221313839c8a7c2b685e
Pierre Jamelot <pierre.jamelot@inria.fr>
2021-05-21 14:14:50 +0200

    wip

--- config/armv8a/boards/rpi3/boardsupport.h
+++ config/armv8a/boards/rpi3/boardsupport.h
@@ -14,51 +14,17 @@
 #define BOARDSUPPORT_H
 #include <time.h>
 #include <stdio.h>
+#include <sys/time.h>
 
 #define CPU_MHZ 1
 typedef unsigned int uint32_t;
 
-readticks(unsigned int *result, int enabled)
+
+
+
+volatile inline uint32_t read_time() 
 {
-    struct timeval t;
-    unsigned int cc;
-    unsigned int val;
-    if (!enabled) {
-        // program the performance-counter control-register:
-        asm volatile("msr pmcr_el0, %0" : : "r" (17));
-        //enable all counters
-        asm volatile("msr PMCNTENSET_EL0, %0" : : "r" (0x8000000f));
-        //clear the overflow 
-        asm volatile("msr PMOVSCLR_EL0, %0" : : "r" (0x8000000f));
-        enabled = 1;
-    }
-    //read the coutner value
-    asm volatile("mrs %0, PMCCNTR_EL0" : "=r" (cc));
-    gettimeofday(&t,(struct timezone *) 0);
-    result[0] = cc;
-    result[1] = t.tv_usec;
-    result[2] = t.tv_sec;
-
-volatile inline uint32_t read_time() {
-    unsigned int init[3] = {0};
-    unsigned int start[3] = {0};
-    unsigned int end[3] = {0};
-    unsigned int overhead = 0;
-
-    readticks(init, 0);
-    readticks(start, 1);
-    readticks(end, 1);
-
-    overhead = end[0] - start[0];
-    readticks(init, 0);
-    readticks(start, 1);
-    foo(); //This is my function 
-    readticks(end, 1);
-
-    end[0] = end[0] - start[0] - overhead;
-    printf("clock cycles= %d\n", end[0]);
     return 0;
-return time;
 }
 
 
